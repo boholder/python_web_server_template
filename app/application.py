742f7fdb7c94eb0243fa67b97bc983c7717e0e5e
boholder <bottleholder@anche.no>
2024-03-12 10:42:24 +0800

    Print config info at app starting

--- app/application.py
+++ app/application.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 
 from asgi_correlation_id import CorrelationIdMiddleware, correlation_id
@@ -9,6 +10,8 @@ from app import config
 from app.config import log_config
 from app.routers import demo
 
+log: logging.Logger
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -18,6 +21,9 @@ async def lifespan(_: FastAPI):
     # thus the file handler should exist for reusing.
     log_config.configure_app_logging()
 
+    global log
+    log = logging.getLogger(__name__)
+    log.info(f"Starting application with following configuration: {config.CONFIG}")
     yield
 
 
@@ -31,6 +37,8 @@ async def unhandled_exception_handler(request: Request, exc: Exception) -> Respo
     """
     ref: https://github.com/snok/asgi-correlation-id?tab=readme-ov-file#fastapi-1
     """
+
+    log.error("Caught an unhandled exception", exc_info=exc)
     return await http_exception_handler(
         request, HTTPException(500, "Internal server error", headers={"X-Request-ID": correlation_id.get() or ""})
     )
